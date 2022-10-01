2f017cc76f2702e04f086c914d87d6574f7a127c
Tamara Leiva <t.leivavaldivia@gmail.com>
2022-10-01 00:52:39 -0300

    añadir condiciones al formulario y tooltip en el footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
 
+$(document).ready(function(){
 
-$(document).scroll(function() {
-    const y = $("html").scrollTop();
+    
+    $(function () {
+        $('[data-bs-toggle="tooltip"]').tooltip();
+      });
+      
+    
+    $(document).scroll(function() {
+        const y = $("html").scrollTop();
+    
+        y > 150 ? $("nav").addClass ("navbar-opaco") : $("nav").removeClass("navbar-opaco");
+    
+    })
 
-    y > 150 ? $("nav").addClass ("navbar-opaco") : $("nav").removeClass("navbar-opaco")
+    $("#boton-enviar").click(function() {
+        var nombre= $("#formularioContactoNombre").val().trim();
+        var asunto= $("#formularioContactoAsunto").val().trim();
+        var mensaje= $("#formularioContactoMensaje").val().trim();
 
-})
+        if ( nombre != "" && asunto != "" && mensaje != "" ) {
+            alert("Tu correo fue enviado correctamente, Viajes Chile se contactará contigo a la brevedad.");
+        }else{
+            alert("ERROR! Debes ingresar todos los campos")
+        }
+
+
+    });
+
+   
+    $(".card-title").click(function () {
+        $(".card-text").toggle();
+    });
+   
+
+});
